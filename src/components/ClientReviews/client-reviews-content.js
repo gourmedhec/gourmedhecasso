2ac19e5d8ec22b2198f1877b7449a099753c6787
gourmedhec <gourmedhec@gourmedhec.fr>
2025-08-31 18:06:15 +0200

    first commit

---
+++ src/components/ClientReviews/client-reviews-content.js
@@ -0,0 +1,46 @@
+const clientReviewsContent = [
+  {
+    id: 1,
+    name: "Pôle Logistique",
+    title: "La Log",
+    image: "/client-reviews/client-review-1.jpg",
+    avatar: "/clients/client-1.jpg",
+    review:
+"La Logistique, c’est le fourneau de l’asso ! Dans ce pôle, tu superviseras l’organisation matérielle de nos événements. Ça passe par l’élaboration des recettes, la planification des staff d’event, l’événement. Anticiper les détails et réagir aux imprévus est la vie qu’ont décidé de mener les logueux. Si tu as la fibre organisationnelle et que tu es prêt à arpenter les rayons des supermarchés avec une calculatrice en main, la Log n’attend plus que toi !",  },
+  {
+    id: 2,
+    name: "Pôle Relations Partenaires",
+    title: "Le Spons",
+    image: "/client-reviews/client-review-2.jpg",
+    avatar: "/clients/client-2.jpg",
+    review:
+    "Les Relations Partenaires est le pôle qui permet de faire entrer du biff dans l’asso ! Il est responsable des relations avec les partenaires notamment financiers. Les membres de ce pôle sont indispensables à la tenue de nos events. Comme tu peux l'imaginer, En bref, c'est le pôle qui fait kiffer les participants et les gagnants de nos événements grâce aux lots gourmands. Alors, si tu as du bagout, que tu n'as pas froid aux yeux et que tu aimes discuter avec les gens, rejoins notre équipe !",
+  },
+  {
+    id: 3,
+    name: "Pôle Communiaction & Création",
+    title: "La Kom-Kréa",
+    image: "/client-reviews/client-review-3.jpg",
+    avatar: "/clients/client-3.jpg",
+    review:
+"Le pôle Communication & Création, c’est la vitrine de l’asso. Communication et création sont les maîtres mots. Si le nom du pôle laisse a désirer, notre objectif c’est d’être désirés. Dans ce pôle tu pourras laisser libre cours à ta créativité et réfléchir à des stratégies pour créer les plus gros buzz (sur Facebook...). L’intérêt avec la KK c’est qeu tu es toujours en relation avec les autres pôles pour correspondre à leurs attentes. Ici, tu peux apprendre à maîtriser des logiciels sympas comme la suite Adobe, Figma ou encore Canva.",  },
+  {
+    id: 4,
+    name: "Pôle RSO",
+    title: "La Responsabilité Sociétale",
+    image: "/client-reviews/client-review-4.jpg",
+    avatar: "/clients/client-4.jpg",
+    review:
+      "From first conversation to final delivery, the process was thoughtful and exact. Each stage revealed a design that was both surprising and completely aligned with how we wanted to live.",
+  },
+  {
+    id: 5,
+    name: "Le Bureau",
+    title: "Le Bureau Strict",
+    image: "/client-reviews/client-review-5.jpg",
+    avatar: "/clients/client-5.jpg",
+    review:
+"La présidente doit faire en sorte que tout se déroule au mieux entre les membres. En plus de superviser les projets, elle représente l’association auprès de l’administration et endosse donc l’ensemble des responsabilités.",  },
+];
+
+export default clientReviewsContent;
